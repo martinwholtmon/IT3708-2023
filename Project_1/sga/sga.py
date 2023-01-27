770e19cec2ae5c0607b00304e871c74b509a8c61
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-01-27 19:57:23 +0100

    feat: method to calculate phenotype. Renamed init_pop_size to pop_size

--- Project_1/sga/sga.py
+++ Project_1/sga/sga.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 class Individual:
     """This class represent an individual in a population"""
 
@@ -8,9 +11,13 @@ class Individual:
         parents: "list[Individual]" = None,
     ) -> None:
         self.bitstring = bitstring
-        self.phenotype = phenotype
         self.fitness = fitness
         self.parents = parents or []
+        self.phenotype: int = 0
+
+    def calc_phenotype(self) -> None:
+        """Will calculate the phenotype (binary to integer) given the individuals bitstring."""
+        self.phenotype = int("".join(map(str, self.bitstring)), 2)
 
 
 class Population:
@@ -32,13 +39,13 @@ class SGA:
 
     def __init__(
         self,
-        init_pop_size: int = 1000,
+        pop_size: int = 1000,
         individual_size: int = 15,
         max_generations: int = 0,
         crossover_rate: float = 0,
         mutation_rate: float = 0,
     ) -> None:
-        self.init_size = init_pop_size
+        self.pop_size = pop_size
         self.individual_size = individual_size
         self.max_generations = max_generations
         self.crossover_rate = crossover_rate
