6eb426aaffde49069ccc3e193a0e35ff7253579c
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-01-27 17:49:50 +0100

    implemented the classes to represent an individual, population and the SGA

--- Project_1/sga/__init__.py
+++ Project_1/sga/__init__.py
@@ -4,3 +4,7 @@
 def main():
     """Entry point for the application script"""
     print("Call your main application code here")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Project_1/sga/sga.py
@@ -0,0 +1,41 @@
+class Individual:
+    def __init__(
+        self,
+        bitstring: list[int],
+        phenotype: str,
+        fitness: float = 0,
+        parents: "list[Individual]" = None,
+    ) -> None:
+        self.bitstring = bitstring
+        self.phenotype = phenotype
+        self.fitness = fitness
+        self.parents = parents or []
+
+
+class Population:
+    def __init__(
+        self,
+        individuals: "list[Individual]" = None,
+        prev_gen: "Population" = None,
+        generation_nr: int = 0,
+    ) -> None:
+        self.individuals = individuals or []
+        self.prev_gen = prev_gen
+        self.generation_nr = generation_nr
+
+
+class SGA:
+    def __init__(
+        self,
+        init_pop_size: int = 1000,
+        individual_size: int = 15,
+        max_generations: int = 0,
+        crossover_rate: float = 0,
+        mutation_rate: float = 0,
+    ) -> None:
+        self.init_size = init_pop_size
+        self.individual_size = individual_size
+        self.max_generations = max_generations
+        self.crossover_rate = crossover_rate
+        self.mutation_rate = mutation_rate
+        self.generations = {}
