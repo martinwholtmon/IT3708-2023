fee62096b5ae075b68fe015154169558460e360d
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-02-09 14:37:58 +0100

    fix: transform the fitness value using the sin function

--- Project_1/src/fitnessfunction.py
+++ Project_1/src/fitnessfunction.py
@@ -1,11 +1,12 @@
 """The fitness (objective) functions that operate on the individuals
 """
+import math
 from sga import Population
 
 
 def sine(population: Population) -> float:
-    """calculate the fitness value in the range [0, 1]
-    scaling factor = upper bound / max value
+    """calculate the fitness value in the interval [0, 128]
+    scaling factor = ((upper_bound - lower_bound) / max_value) + lower_bound
 
     Returns:
         float: fitness value
@@ -13,7 +14,7 @@ def sine(population: Population) -> float:
     scaling_factor = 2 ** (-8)
     for individual in population.individuals:
         phenotype = int("".join(map(str, individual.bitstring)), 2)
-        individual.fitness = phenotype * scaling_factor
+        individual.fitness = math.sin(phenotype * scaling_factor)
 
 
 def lin_reg(population: Population) -> float:
