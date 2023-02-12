fa6ebba4a7506782bbd06ba34e237a8ec3b1bec8
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-02-12 18:37:51 +0100

    load the dataset and prepare the sga for simulation using the dataset

--- Project_1/test.py
+++ Project_1/test.py
@@ -1,5 +1,6 @@
 from src.fitnessfunction import ObjectiveSine, ObjectiveLinReg
 from src.sga import SGA
+import numpy as np
 
 
 def ss_sine():
@@ -17,5 +18,28 @@ def ss_sine():
     print(solution.bitstring, solution.value, solution.fitness)
 
 
+def ss_linreg():
+    # Load dataset
+    data_path = r"./data/dataset.txt"
+    df = np.genfromtxt(data_path, delimiter=",")
+    y = df[:, -1]
+    X = np.delete(df, 0, -1)
+
+    # Prepare params
+    params = {
+        "objective_function": ObjectiveLinReg(X, y).get_fitness,
+        "pop_size": 15,
+        "individual_size": X.shape[1],
+        "max_generations": 0,
+        "crossover_rate": 0.6,
+        "mutation_rate": 0.05,
+    }
+
+    sga = SGA(**params)
+    solution = sga.simulate()
+    print(solution.bitstring, solution.value, solution.fitness)
+
+
 if __name__ == "__main__":
-    ss_sine()
+    # ss_sine()
+    ss_linreg()
