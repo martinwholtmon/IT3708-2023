f81e858c1762212af125a0adcbbbdc475ed638d3
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-02-13 03:16:08 +0100

    refactor: Moved population and individual into own files

---
+++ Project_1/src/population.py
@@ -0,0 +1,21 @@
+class Population:
+    """This class represent a population in the SGA"""
+
+    def __init__(
+        self,
+        individuals: "list[Individual]" = None,
+        prev_gen: "Population" = None,
+        generation_nr: int = 0,
+    ) -> None:
+        self.individuals = individuals or []
+        self.prev_gen = prev_gen
+        self.generation_nr = generation_nr
+
+    def __str__(self):
+        return (
+            f"Generation {self.generation_nr} avg. fitness: {self.__calc_avg_fitness()}"
+        )
+
+    def __calc_avg_fitness(self):
+        fitness = sum([individual.fitness for individual in self.individuals])
+        return fitness / len(self.individuals)
--- Project_1/src/sga.py
+++ Project_1/src/sga.py
@@ -1,43 +1,8 @@
+from src.individual import Individual
+from src.population import Population
 import numpy as np
 import copy
 from random import random, sample
-
-
-class Individual:
-    """This class represent an individual in a population"""
-
-    def __init__(
-        self,
-        bitstring: np.ndarray[int],
-        parents: "list[Individual]" = None,
-    ) -> None:
-        self.bitstring = bitstring
-        self.parents = parents or []
-        self.value: float = 0
-        self.fitness: float = 0
-
-
-class Population:
-    """This class represent a population in the SGA"""
-
-    def __init__(
-        self,
-        individuals: "list[Individual]" = None,
-        prev_gen: "Population" = None,
-        generation_nr: int = 0,
-    ) -> None:
-        self.individuals = individuals or []
-        self.prev_gen = prev_gen
-        self.generation_nr = generation_nr
-
-    def __str__(self):
-        return (
-            f"Generation {self.generation_nr} avg. fitness: {self.__calc_avg_fitness()}"
-        )
-
-    def __calc_avg_fitness(self):
-        fitness = sum([individual.fitness for individual in self.individuals])
-        return fitness / len(self.individuals)
 
 
 class SGA:
