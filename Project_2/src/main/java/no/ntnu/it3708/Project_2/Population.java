18a48d77b90d46b7e78671a9699efa97b6188150
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-03-07 12:14:21 +0100

    feat: added Population class to represent a set of nurses

--- Project_2/src/main/java/no/ntnu/it3708/Project_2/Population.java
+++ Project_2/src/main/java/no/ntnu/it3708/Project_2/Population.java
@@ -1,4 +1,47 @@
 package no.ntnu.it3708.Project_2;
 
+import java.util.ArrayList;
+
 public class Population {
+    private ArrayList<Individual> feasible_individuals;
+    private ArrayList<Individual> infeasible_individuals;
+    private final Population old_pop;
+    private final Integer generation_nr;
+
+    public Population() {
+        this.feasible_individuals = new ArrayList<>();
+        this.infeasible_individuals = new ArrayList<>();
+        this.old_pop = null;
+        this.generation_nr = 0;
+    }
+
+    public Population(ArrayList<Individual> feasible_individuals, ArrayList<Individual> infeasible_individuals, Population old_pop, Integer generation_nr) {
+        this.feasible_individuals = feasible_individuals;
+        this.infeasible_individuals = infeasible_individuals;
+        this.old_pop = old_pop;
+        this.generation_nr = generation_nr;
+    }
+
+    public ArrayList<Individual> getFeasible_individuals() {
+        return feasible_individuals;
+    }
+
+    public ArrayList<Individual> getInfeasible_individuals() {
+        return infeasible_individuals;
+    }
+
+    public Integer getGeneration_nr() {
+        return generation_nr;
+    }
+
+    @Override
+    public String toString() {
+        return "Generation " + generation_nr + " avg. fitness: " + calc_avg_fitness();
+    }
+
+    private Float calc_avg_fitness() {
+        Float total_fitness = 0f;
+        for (Individual i : feasible_individuals) total_fitness += i.getFitness();
+        return total_fitness / feasible_individuals.size();
+    }
 }
