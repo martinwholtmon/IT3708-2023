c88b3e1e0dc8611f140213fdf0e4d6866fad27ce
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-03-07 12:18:50 +0100

    feat: created a main to connect the different elements to run the SGA

--- Project_2/src/main/java/no/ntnu/it3708/Project_2/Main.java
+++ Project_2/src/main/java/no/ntnu/it3708/Project_2/Main.java
@@ -9,13 +9,17 @@ public class Main {
         DataHandler data = new DataHandler();
         data.loadData("/data/train_0.json");
 
-        // Prepare fitness function
-
-
         // Prepare params
-
+        ObjectiveFunction objectiveFunction = new ObjectiveFunction(data);
+        boolean maximize = false;
+        int pop_size = data.getNbr_nurses();
+        int individual_size = data.getPatients().size();
+        int max_generations = 5;
+        float crossover_rate = 0.6f;
+        float mutation_rate = 0.05f;
 
         // Execute the SGA
-
+        SGA sga = new SGA(objectiveFunction, maximize, pop_size, individual_size, max_generations, crossover_rate, mutation_rate);
+//        sga.simulate();
     }
 }
