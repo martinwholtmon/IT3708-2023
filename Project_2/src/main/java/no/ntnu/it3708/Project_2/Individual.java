aedfbdde8bbf49eab711fa7f14dca9eef5f5f564
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-03-07 12:11:30 +0100

    feat: added individual class to represent a nurse

--- Project_2/src/main/java/no/ntnu/it3708/Project_2/Individual.java
+++ Project_2/src/main/java/no/ntnu/it3708/Project_2/Individual.java
@@ -1,4 +1,46 @@
 package no.ntnu.it3708.Project_2;
 
+import java.util.ArrayList;
+
 public class Individual {
+    private final ArrayList<Integer> bitstring;
+    private ArrayList<Individual> parents;
+    private Float fitness;
+
+
+    public Individual(ArrayList<Integer> bitstring) {
+        this.bitstring = bitstring;
+        this.parents = new ArrayList<>();
+        this.fitness = 0f;
+    }
+
+    public Individual(ArrayList<Integer> bitstring, ArrayList<Individual> parents, Float fitness) {
+        this.bitstring = bitstring;
+        this.parents = parents;
+        this.fitness = fitness;
+    }
+
+    public ArrayList<Individual> getParents() {
+        return parents;
+    }
+
+    public void setParents(ArrayList<Individual> parents) {
+        this.parents = parents;
+    }
+
+    public Float getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(Float fitness) {
+        this.fitness = fitness;
+    }
+
+    @Override
+    public String toString() {
+        return "Individual{" +
+                "bitstring=" + bitstring +
+                ", fitness=" + fitness +
+                '}';
+    }
 }
