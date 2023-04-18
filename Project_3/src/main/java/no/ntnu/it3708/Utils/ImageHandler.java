da9b6552ed1ac84574d0b8e2ab94e58afd7c491a
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-04-18 20:12:17 +0200

    refactor: extracted the function to create 2d array from buffered image

--- Project_3/src/main/java/no/ntnu/it3708/Utils/ImageHandler.java
+++ Project_3/src/main/java/no/ntnu/it3708/Utils/ImageHandler.java
@@ -16,13 +16,30 @@ public class ImageHandler {
     public ImageHandler() {
     }
 
+    /**
+     * Load an image from the resource folder given an image_id.
+     * It will locate the correct folder, and then load the Test image.jpg
+     *
+     * @param image_id id of folder/image
+     * @return image as a 2d array
+     * @throws IOException
+     * @throws URISyntaxException
+     */
     public int[][] loadImage(int image_id) throws IOException, URISyntaxException {
         // Read image file
         String path = IMAGE_BASE_FOLDER + image_id + "/" + IMAGE_NAME;
         String filename = getClass().getResource(path).toURI().getPath();
         File imageFile = new File(filename);
         BufferedImage image = ImageIO.read(imageFile);
+        return buffImageTo2d(image);
+    }
 
+    /***
+     * Will create a 2d array from a buffered image
+     * @param image buffered image
+     * @return 2d array of image
+     */
+    private int[][] buffImageTo2d(BufferedImage image) {
         // Convert it to usable format: 2d-array
         int width = image.getWidth();
         int height = image.getHeight();
@@ -34,7 +51,6 @@ public class ImageHandler {
                 pixelArray[x][y] = pixel;
             }
         }
-
         return pixelArray;
     }
 }
