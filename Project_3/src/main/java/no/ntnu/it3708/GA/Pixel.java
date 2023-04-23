04495288a96c4f87cad338ac44c347a0acb0bb06
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-04-23 13:02:54 +0200

    feat: defined a pixel

--- Project_3/src/main/java/no/ntnu/it3708/GA/Pixel.java
+++ Project_3/src/main/java/no/ntnu/it3708/GA/Pixel.java
@@ -1,7 +1,28 @@
 /**
- * This is the genotype (chromosome) which represent a pixel in the image.
+ * Represent a pixel in the image.
  */
 package no.ntnu.it3708.GA;
 
+import java.awt.*;
+import java.util.HashMap;
+
 public class Pixel {
+    private final int id;
+    private int x;
+    private int y;
+    private Color color;
+    private HashMap<Integer, Pixel> neighbors;  // 1-8 representing the neighbors: East-West-North-South east-west
+
+    public Pixel(int id, int x, int y, Color color) {
+        this.id = id;
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.neighbors = new HashMap<>();
+
+        // init hashmap for the eight neighbors
+        for (int i = 1; i <= 8; i++) {
+            this.neighbors.put(i, null);
+        }
+    }
 }
