467a956be992138bd948776f43c4fa0d09d51e94
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-04-28 20:05:50 +0200

    chore: javadoc

--- Project_3/src/main/java/no/ntnu/it3708/Utils/utils.java
+++ Project_3/src/main/java/no/ntnu/it3708/Utils/utils.java
@@ -2,6 +2,9 @@ package no.ntnu.it3708.Utils;
 
 import java.util.Random;
 
+/**
+ * The type Utils.
+ */
 public final class utils {
     private static Random RANDOM = new Random();
 
@@ -11,8 +14,9 @@ public final class utils {
 
     /**
      * Return a random int from 0 to upper
-     * 
+     *
      * @param upper limit
+     * @return the int
      */
     public static int randomInt(int upper) {
         if (upper == 0)
@@ -22,9 +26,10 @@ public final class utils {
 
     /**
      * Return random int between upper and lower
-     * 
+     *
      * @param lower limit
      * @param upper limit
+     * @return the int
      */
     public static int randomInt(int lower, int upper) {
         return lower + RANDOM.nextInt(upper - lower + 1);
@@ -32,7 +37,7 @@ public final class utils {
 
     /**
      * Generate a random double between 0-1 (including)
-     * 
+     *
      * @return random double [0,1]
      */
     public static double randomDouble() {
@@ -42,10 +47,10 @@ public final class utils {
 
     /**
      * Generate random double between the boundaries
-     * 
+     *
      * @param lower limit
      * @param upper limit
-     * @return
+     * @return double
      */
     public static double randomDouble(int lower, int upper) {
         return lower + (upper - lower) * RANDOM.nextDouble();
