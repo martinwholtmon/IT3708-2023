a147c291956c3546349c204484947268efcdf227
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-04-28 20:09:32 +0200

    feat: generate the initial segments for an idividual

--- Project_3/src/main/java/no/ntnu/it3708/GA/Individual.java
+++ Project_3/src/main/java/no/ntnu/it3708/GA/Individual.java
@@ -3,8 +3,14 @@
  */
 package no.ntnu.it3708.GA;
 
-import java.util.List;
+import no.ntnu.it3708.Parameters;
+import no.ntnu.it3708.Utils.utils;
 
+import java.util.*;
+
+/**
+ * The type Individual.
+ */
 public class Individual {
     private List<Segment> segments;
     private double fitness;
@@ -13,16 +19,17 @@ public class Individual {
     private double deviation;
 
     /**
-     * Generate an individual
+     * Generate an initial individual
      */
     public Individual() {
-        this.segments = generateInitialSegments();
+        this.segments = new ArrayList<>();
+        generateInitialSegments();
         calculateObjectiveFunctions();
         calculateFitness();
     }
 
     /**
-     * Given a list of segments, generate individual
+     * Given a list of segments, creates the individual
      *
      * @param segments set of pixels as segments
      */
@@ -33,12 +40,48 @@ public class Individual {
     }
 
     /**
-     * Generate the initial segments for a new/initial individual
-     *
-     * @return list of segments
+     * Generate the initial segments for a new/initial individual.
+     * From the paper "A Multi-objective Evolutionary Algorithm for Color Image
+     * Segmentation", is recommended to construct the initial population using MST
+     * (Minimum spanning tree).
      */
-    private List<Segment> generateInitialSegments() {
-        return null;
+    private void generateInitialSegments() {
+        Map<Integer, Segment> visitedPixels = new HashMap<>();
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+
+        // Pick random segment size between the GA boundaries
+        int numberOfSegments = utils.randomInt(Parameters.MIN_SEGMENTS, Parameters.MAX_SEGMENTS);
+
+        // Initialize segments with random root pixel
+        for (int i = 0; i < numberOfSegments; i++) {
+            Segment segment = new Segment();
+            segments.add(segment);
+
+            // Pick a random "root" pixel that has not been visited yet
+            Pixel rootPixel = GA.pixels.get(utils.randomInt(GA.pixels.size()));
+            while (visitedPixels.containsKey(rootPixel.getId())) {
+                rootPixel = GA.pixels.get(utils.randomInt(GA.pixels.size()));
+            }
+            visitedPixels.put(rootPixel.getId(), segment);
+            segment.addPixels(rootPixel.getId(), rootPixel);
+            pq.addAll(rootPixel.getNeighbors());
+        }
+
+        // add nodes
+        while (!pq.isEmpty()) {
+            Node edge = pq.poll();
+            Pixel pixel = edge.getPixel();
+            Pixel neighPixel = edge.getNeighboringPixel();
+
+            if (!visitedPixels.containsKey(neighPixel.getId())) {
+                Segment segment = visitedPixels.get(pixel.getId());
+                visitedPixels.put(neighPixel.getId(), segment);
+                segment.addPixels(neighPixel.getId(), neighPixel);
+
+                // add neighbors to queue
+                pq.addAll(neighPixel.getNeighbors());
+            }
+        }
     }
 
     /**
