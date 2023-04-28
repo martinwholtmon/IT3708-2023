1a2b383d50606b5265bb56a1c8aa6292166cb623
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-04-28 19:53:00 +0200

    feat: intensity and colordistance functions

--- Project_3/src/main/java/no/ntnu/it3708/Utils/ImageHandler.java
+++ Project_3/src/main/java/no/ntnu/it3708/Utils/ImageHandler.java
@@ -55,4 +55,33 @@ public final class ImageHandler {
         }
         return pixelArray;
     }
+
+    /**
+     * Will calculate the monochrome luminance of the color as an intensity
+     * according to NTSC formula: Y = 0.299*r + 0.587*g + 0.114*b
+     *
+     * @param color The color to convert
+     * @return the monochrome luminance [0.0, 255.0]
+     */
+    public static double intensity(Color color) {
+        int r = color.getRed();
+        int g = color.getGreen();
+        int b = color.getBlue();
+        if (r == g && r == b)
+            return r;
+        return 0.299 * r + 0.587 * g + 0.114 * b;
+    }
+
+    /**
+     * Calculates the euclidean color distance
+     *
+     * @param c1 color 1
+     * @param c2 color 2
+     * @return the distance
+     */
+    public static double colorDistance(Color c1, Color c2) {
+        return Math.sqrt(Math.pow((double) c1.getRed() - c2.getRed(), 2) +
+                Math.pow((double) c1.getGreen() - c2.getGreen(), 2) +
+                Math.pow((double) c1.getBlue() - c2.getBlue(), 2));
+    }
 }
