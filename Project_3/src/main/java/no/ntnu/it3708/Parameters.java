8bf8cfcf8d77342f57d6b3b3ce3e5f1ff751ab4c
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-05-01 17:13:12 +0200

    feat: updated parameters

--- Project_3/src/main/java/no/ntnu/it3708/Parameters.java
+++ Project_3/src/main/java/no/ntnu/it3708/Parameters.java
@@ -5,29 +5,25 @@ package no.ntnu.it3708;
  */
 public final class Parameters {
 
-    private Parameters() {
-    }
-
     /**
      * The constants for the image handler
      */
     public static final String IMAGE_FOLDER = "86016";
     public static final String IMAGE_BASE_FOLDER = "training_images";
     public static final String IMAGE_NAME = "Test image.jpg";
-
     /**
      * The constants for the GA
      */
-    public static final Integer POP_SIZE = 1;
-
-    public static final Integer MAX_GENERATIONS = 100;
-    public static final Float CROSSOVER_RATE = 0.6f;
-    public static final Float MUTATION_RATE = 0.1f;
-
+    public static final Integer POP_SIZE = 50;
+    public static final Integer MAX_GENERATIONS = 1; // set to 100
+    public static final Float CROSSOVER_RATE = 0.7f;
+    public static final Float MUTATION_RATE = 0.2f;
+    public static final Integer TOURNAMENET_SIZE = 4;
     /**
      * The constants for segmentations
      */
     public static final int MIN_SEGMENTS = 3;
-
     public static final int MAX_SEGMENTS = 15;
+    private Parameters() {
+    }
 }
